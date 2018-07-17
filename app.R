101be40527f1c80e2e6b5a8ae08f76f648895d8c
wesslen <rwesslen@gmail.com>
2018-07-17 14:08:56 -0400

    modify DT to rank by rt and likes automatically

--- app.R
+++ app.R
@@ -238,7 +238,8 @@ server <- function(input, output) {
   rownames = FALSE, 
   colnames = c("Timestamp", "User", "Tweet", "RT", "Likes", "Mentioned"), 
   filter = 'top',
-  options = list(lengthMenu = c(5, 10, 25, 50, 100), pageLength = 25)
+  order = list(list(4, 'desc'), list(5, 'desc'))
+  options = list(lengthMenu = c(5, 10, 25, 50, 100), pageLength = 5)
   )
   
   output$tweet <- renderText({
